c855e7058e524f8aa88335ee294ca404e032859b
Felosi <97246974+FelosiDev@users.noreply.github.com>
2023-06-15 23:19:18 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  token: process.env.token, 
+  port: "3000",
+  prefix: "+",
+  client: "Oa2",
+  client_id: "1090059808872341606",
+  client_secret: "5zlKEuT38_n8DzTNRJqsrhMxRjycMLpM",
+  redirect_uri: "https://restorecord.com/api/callback",
+  footer: "[+] Protect",
+  support: "https://discord.gg/oa2",
+  owners: "779716357872680970",
+  authLink: "https://discord.com/oauth2/authorize?client_id=1090059808872341606&redirect_uri=https://restorecord.com/api/callback&response_type=code&scope=identify+guilds.join&state=1093018998414778399",
+
+}
+
+{ "by Felosi" }
