6506b562221b6283acabd354ce495efeabcbbbf2
Felosi <97246974+FelosiDev@users.noreply.github.com>
2024-02-09 08:45:40 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+authlink: "https://discord.com/oauth2/authorize?client_id=1205140961269645372&redirect_uri=https://restorecord.com/api/callback&response_type=code&scope=identify+guilds.join&state=1204115235930247318"
+}
